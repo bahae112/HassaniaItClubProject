b3e9aec4fc6ac058428f9bfd97df03c1ece0bfed
bahae112 <bahaeaouanet2004@gmail.com>
2025-03-31 12:48:24 +0200

    added

---
+++ model/Messages.php
@@ -0,0 +1,68 @@
+<?php
+
+class Messages {
+    private $id; // Ajout de l'ID (si vous utilisez une base de données)
+    private $nom;
+    private $email;
+    private $objet;
+    private $message;
+    private $dateEnvoie;
+
+    public function __construct($nom, $email, $objet, $message, $dateEnvoie) {
+        $this->nom = $nom;
+        $this->email = $email;
+        $this->objet = $objet;
+        $this->message = $message;
+        $this->dateEnvoie = $dateEnvoie;
+    }
+
+    // Getters
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getObjet() {
+        return $this->objet;
+    }
+
+    public function getMessage() {
+        return $this->message;
+    }
+
+    public function getDateEnvoie() {
+        return $this->dateEnvoie;
+    }
+
+    // Setters
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setObjet($objet) {
+        $this->objet = $objet;
+    }
+
+    public function setMessage($message) {
+        $this->message = $message;
+    }
+
+    public function setDateEnvoie($dateEnvoie) {
+        $this->dateEnvoie = $dateEnvoie;
+    }
+}
---
+++ model/Utilisateurs.php
@@ -0,0 +1,79 @@
+<?php
+
+class Utilisateurs {
+    private $nom;
+    private $prenom;
+    private $role;
+    private $telephone;
+    private $datePriseDeFonction;
+    private $email;
+    private $motDePasse;
+
+    public function __construct($nom, $prenom, $role, $telephone, $datePriseDeFonction, $email, $motDePasse) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->role = $role;
+        $this->telephone = $telephone;
+        $this->datePriseDeFonction = $datePriseDeFonction;
+        $this->email = $email;
+        $this->motDePasse = $motDePasse;
+    }
+
+    // Getters
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+
+    public function getTelephone() {
+        return $this->telephone;
+    }
+
+    public function getDatePriseDeFonction() {
+        return $this->datePriseDeFonction;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getMotDePasse() {
+        return $this->motDePasse;
+    }
+
+    // Setters
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+
+    public function setRole($role) {
+        $this->role = $role;
+    }
+
+    public function setTelephone($telephone) {
+        $this->telephone = $telephone;
+    }
+
+    public function setDatePriseDeFonction($datePriseDeFonction) {
+        $this->datePriseDeFonction = $datePriseDeFonction;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function setMotDePasse($motDePasse) {
+        $this->motDePasse = $motDePasse;
+    }
+}
